bb42d01f97252fe2a1a849d23ad9e7c20fb8bb15
NAMBIRAJA M <nambirajam.ug.21.cs@scadengineering.ac.in>
2024-10-09 20:16:26 +0530

    created a user and added into database

--- mongoose-basics/app.js
+++ mongoose-basics/app.js
@@ -1,6 +1,7 @@
 
 const express=require('express');
 const mongoose =require('mongoose');
+const User=require('./Models/userModel')
 
 const app=express();
 mongoose.connect('mongodb://127.0.0.1:27017/Sampledata',{
@@ -9,5 +10,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/Sampledata',{
 })
 .then(()=>console.log("database connected successfully..!"))
 .catch((err)=>console.log("error from database connection",err));
+
+
+/* create */
+
+const newUser = new User({
+    name:"john doe",
+    age:20,
+    email:"john@gmail.com"
+})
+
+newUser.save()
+.then(()=>console.log("user saved!"))
+.catch(err =>console.log('error'))
+
 
 app.listen(8000,()=>{console.log('Listening to 8000 port');})
